5a7c34d76f85e74d2d0bfda5948c13bc540ad060
assis1983 <assis.ederjd@gmail.com>
2023-12-13 11:41:39 -0300

    add images from the skiils session

--- Portifoliio/src/components/page/index.tsx
+++ Portifoliio/src/components/page/index.tsx
@@ -10,6 +10,14 @@ import {
 } from "./style";
 import IconJs from "../../assets/js";
 import IconNode from "../../assets/node";
+import IconTS from "../../assets/ts";
+import IconNpm from "../../assets/npm";
+import IconReact from "../../assets/react";
+import IconHtml from "../../assets/html";
+import IconCss from "../../assets/css";
+import IconVscode from "../../assets/vscode";
+import IconMongodb from "../../assets/mondodb";
+import IconGit from "../../assets/git";
 
 const Page = () => {
   return (
@@ -30,6 +38,38 @@ const Page = () => {
         <div>
           <IconNode />
           <p>NODE</p>
+        </div>
+        <div>
+          <IconTS />
+          <p>TYPESCRIPT</p>
+        </div>
+        <div>
+          <IconNpm />
+          <p>NPM</p>
+        </div>
+        <div>
+          <IconReact />
+          <p>REACT</p>
+        </div>
+        <div>
+          <IconHtml />
+          <p>HTML</p>
+        </div>
+        <div>
+          <IconCss />
+          <p>CSS</p>
+        </div>
+        <div>
+          <IconMongodb />
+          <p>MONGODB</p>
+        </div>
+        <div>
+          <IconGit />
+          <p>GIT</p>
+        </div>
+        <div>
+          <IconVscode />
+          <p>VSCODE</p>
         </div>
       </StyleImage>
       <GetKnow id="projects">PROJETOS</GetKnow>
